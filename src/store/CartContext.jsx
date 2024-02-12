ff3335110d28ec792e8ca5271508f690a77843de
Gosia Magdzik <melassa26@gmail.com>
2024-02-12 12:53:08 +0100

    updatedItems array

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,7 +1,43 @@
-import { createContext } from "react"; 
+import { createContext, useReducer } from "react"; 
 
 const CartContext = createContext({
     items: [],
     addItem: (item) => {},
     removeItem: (id) => {}
 });
+
+function cartReducer(state, action) {
+    if (action.type === 'ADD_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id); //if did not find item return -1
+
+        const updatedItems = [...state.items]; //copy of old array 
+
+        if (existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
+    }
+
+    if (action.type === 'REMOVE_ITEM') {
+        // ... Update the state to REMOVE a meal item
+    }
+
+    return state; //return unchanged state
+
+}
+
+export function CartContextProvider({ children }) {
+    useReducer(cartReducer, { items: [] });
+
+    return <CartContext.Provider>{children}</CartContext.Provider>
+}
+
+export default CartContext;

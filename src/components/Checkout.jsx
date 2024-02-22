e1003d8effb83696856d76aa986cce0363bdd3ea
Gosia Magdzik <melassa26@gmail.com>
2024-02-22 17:21:03 +0100

    Total Amount added + new Input component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react';
 import Modal from './UI/Modal';
 import CartContext from '../store/CartContext';
+import { currencyFormatter } from '../util/formatting'; 
 
 export const Checkout = () => {
-    useContext(CartContext);
+    const cartCtx = useContext(CartContext);
 
-  return (
-    <Modal>
-        <form>
-            <h2>Checkout</h2>
-            <p>Total Amount: </p>
-        </form>
-    </Modal>
-  )
+    const cartTotal = cartCtx.items.reduce(
+        (totalPrice, item) => totalPrice + item.quantity * item.price, 0
+    );
+
+    return (
+        <Modal>
+            <form>
+                <h2>Checkout</h2>
+                <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+            </form>
+        </Modal>
+    )
 }

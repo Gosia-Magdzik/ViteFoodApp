c064d1dd2ef76965ef63728103737d06aecf8afa
Gosia Magdzik <melassa26@gmail.com>
2024-03-04 17:17:59 +0100

    preventing infinite loop

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 async function sendHttpRequest(url, config) {
     const response = await fetch(url, config);
@@ -10,22 +10,22 @@ async function sendHttpRequest(url, config) {
     }
 }
 
-export default function useHttp() {
+export default function useHttp(url, config) {
 
     const [data, setData] = useState()
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
-    async function sendRequest() {
+    const sendRequest = useCallback(async function sendRequest() {
         setIsLoading(true);
         try {
-            const resData = sendHttpRequest();
+            const resData = sendHttpRequest(url, config);
             setData(resData);
         } catch(error) {
             setError(error.message || 'Something went wrong')
         }
         setIsLoading(false);
-    }
+    }, [url, config])
 
     useEffect(() => {
         sendRequest();

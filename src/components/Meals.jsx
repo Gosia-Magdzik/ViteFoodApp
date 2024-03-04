8f69dfc42a2111603e9ef4d88db90bd2f5a670f3
Gosia Magdzik <melassa26@gmail.com>
2024-03-04 18:45:56 +0100

    map'ing error

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,19 +1,24 @@
 import { MealItem } from './MealItem';
 import useHttp from '../hooks/useHttp';
 
+const requestConfig = {};
+
 export const Meals = () => {
     const { 
         data: loadedMeals, 
         isLoading, 
         error 
-    } = useHttp('http://localhost:3000/meals');
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  return (
+    if (isLoading) {
+        return <p>Fetching meals...</p>
+    }
+
+    return (
     <ul id="meals">
-        {loadedMeals.map(meal => 
+        {loadedMeals && loadedMeals.map((meal) => (
             <MealItem key={meal.id} meal={meal}/>
-        )}
-        
+        ))}
     </ul>
-    )
+    );
 }
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -10,28 +10,29 @@ async function sendHttpRequest(url, config) {
     }
 }
 
-export default function useHttp(url, config) {
+export default function useHttp(url, config, initialData) {
 
-    const [data, setData] = useState()
+    const [data, setData] = useState(initialData);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
     const sendRequest = useCallback(async function sendRequest() {
         setIsLoading(true);
         try {
-            const resData = sendHttpRequest(url, config);
+            const resData = await sendHttpRequest(url, config);
             setData(resData);
         } catch(error) {
             setError(error.message || 'Something went wrong')
         }
         setIsLoading(false);
-    }, [url, config])
+        },  
+        [url, config]
+    );
 
     useEffect(() => {
-        if (config && config.method === 'GET') {
+        if (config && (config.method === 'GET' || !config.method) || !config) {
             sendRequest();
         }
-        
     }, [sendRequest, config]);
 
     return {
@@ -39,5 +40,5 @@ export default function useHttp(url, config) {
         isLoading,
         error,
         sendRequest
-    }
+    };
 }

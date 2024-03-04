d8eac460ffde29fff4112b347fa3ab1ca067f55a
Gosia Magdzik <melassa26@gmail.com>
2024-03-04 15:52:02 +0100

    hook, function  updating state based on the request status

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 async function sendHttpRequest(url, config) {
     const response = await fetch(url, config);
 
+    const resData = await response.json();
+
     if (!response.ok) {
-        throw new Error('Something went wrong, failed to send request')
+        throw new Error(resData.message ||'Something went wrong, failed to send request')
     }
 }
 
 export default function useHttp() {
 
+    const [data, setData] = useState()
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    async function sendRequest() {
+        setIsLoading(true);
+        try {
+            const resData = sendHttpRequest();
+            setData(resData);
+        } catch(error) {
+            setError(error.message || 'Something went wrong')
+        }
+        setIsLoading(false);
+    }
+
+    return {
+        data,
+        isLoading,
+        error
+    }
 }
